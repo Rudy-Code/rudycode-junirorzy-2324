7323c6ea28e95617bd18dfeb39c2faff57c99b38
Marek Gondek <marek.gondek@rax.com.pl>
2023-09-22 13:16:01 +0200

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+const players = document.querySelectorAll('.team__player')
+
+const daysCount = document.querySelector('#days')
+const hoursCount = document.querySelector('#hours')
+const minutesCount = document.querySelector('#minutes')
+const secondsCount = document.querySelector('#seconds')
+
+let userTime
+
+const setTime = () => {
+    const currentTime = new Date()
+    const result = userTime - currentTime
+
+    const days = Math.floor(result / 1000 / 60 / 60 / 24)
+	const hours = Math.floor(result / 1000 / 60 / 60) % 24
+	const minutes = Math.floor(result / 1000 / 60) % 60
+	const seconds = Math.floor(result / 1000) % 60
+
+    daysCount.textContent = days
+	hoursCount.textContent = hours
+	minutesCount.textContent = minutes
+	secondsCount.textContent = seconds
+}
+
+const appUpdate = () => {
+	userTime = new Date(`10 1 2023 19:00:00`)
+	setTime()
+}
+
+appUpdate()
+setInterval(setTime, 1000)
